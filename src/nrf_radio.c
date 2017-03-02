39ce644a79892de1138bc6aadd6f7f1843312723
wzl <wzl@badlyby.com>
2017-03-02 16:18:04 +0800

    修复nrf51822射频通讯bug

--- src/nrf_radio.c
+++ src/nrf_radio.c
@@ -26,11 +26,11 @@ CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 #include "nrf.h"
 #include "nrf_delay.h"
 
-#define PACKET0_S1_SIZE                  (0UL)  //!< S1 size in bits
 #define PACKET0_S0_SIZE                  (0UL)  //!< S0 size in bits
-#define PACKET0_PAYLOAD_SIZE             (0UL)  //!< payload size in bits
+#define PACKET0_LEN_SIZE								 (8UL)  //!< LENGTH size in bits
+#define PACKET0_S1_SIZE                  (0UL)  //!< S1 size in bits
 #define PACKET1_BASE_ADDRESS_LENGTH      (4UL)  //!< base address length in bytes
-#define PACKET1_STATIC_LENGTH            (255UL)  //!< static length in bytes
+#define PACKET1_MAX_LENGTH               (255UL)  //!< static length in bytes
 #define PACKET1_PAYLOAD_SIZE             (255UL)  //!< payload size in bytes
 
 #define RADIO_TXMODE      1
@@ -73,7 +73,7 @@ void Radio_SleepMode(void)
     lastmode = RADIO_SLEEPMODE;
 }
 
-uint8_t Radio_SendPacket(uint32_t addrL, uint32_t addrH, uint8_t *data, uint8_t length, uint32_t timeout)
+uint8_t Radio_SendPacket(uint32_t addrL, uint8_t addrH, uint8_t *data, uint8_t length, uint32_t timeout)
 {
     if(lastmode != RADIO_TXMODE) Radio_TXMode();
     NRF_RADIO->BASE0 = addrL;
@@ -84,7 +84,7 @@ uint8_t Radio_SendPacket(uint32_t addrL, uint32_t addrH, uint8_t *data, uint8_t 
                        (RADIO_PCNF1_ENDIAN_Big << RADIO_PCNF1_ENDIAN_Pos)           |
                        (PACKET1_BASE_ADDRESS_LENGTH << RADIO_PCNF1_BALEN_Pos)       |
                        (length << RADIO_PCNF1_STATLEN_Pos)                          |
-                       (PACKET1_PAYLOAD_SIZE << RADIO_PCNF1_MAXLEN_Pos);
+                       (PACKET1_MAX_LENGTH << RADIO_PCNF1_MAXLEN_Pos);
     NRF_RADIO->EVENTS_END  = 0U;//传输完成标志位，复位
     NRF_RADIO->TASKS_START = 1U;//开始传输
     while (NRF_RADIO->EVENTS_END == 0U)//等待传输完成
@@ -99,24 +99,24 @@ uint8_t Radio_SendPacket(uint32_t addrL, uint32_t addrH, uint8_t *data, uint8_t 
 uint8_t Radio_ReceiveData(uint8_t *data, uint8_t length, uint32_t timeout)
 {
     if(lastmode != RADIO_RXMODE) Radio_RXMode();
-    NRF_RADIO->PREFIX0 = NRF_FICR->DEVICEADDR[1];  // Prefix byte of addresses 3 to 0
-    NRF_RADIO->BASE0   = NRF_FICR->DEVICEADDR[0];  // Base address for prefix 0
+    NRF_RADIO->PREFIX0 = NRF_FICR->DEVICEADDR[0];  // Prefix byte of addresses 3 to 0
+		NRF_RADIO->BASE0   = NRF_FICR->DEVICEADDR[1];  // Base address for prefix 0
     NRF_RADIO->PACKETPTR = (uint32_t)data;
     // Packet configuration
     NRF_RADIO->PCNF1 = (RADIO_PCNF1_WHITEEN_Disabled << RADIO_PCNF1_WHITEEN_Pos)    |
                        (RADIO_PCNF1_ENDIAN_Big << RADIO_PCNF1_ENDIAN_Pos)           |
                        (PACKET1_BASE_ADDRESS_LENGTH << RADIO_PCNF1_BALEN_Pos)       |
                        (length << RADIO_PCNF1_STATLEN_Pos)                          |
-                       (PACKET1_PAYLOAD_SIZE << RADIO_PCNF1_MAXLEN_Pos);
-    NRF_RADIO->EVENTS_END = 0U;//传输完成标志位，复位
-    NRF_RADIO->TASKS_START = 1U;//开始传输
-    while(NRF_RADIO->EVENTS_END)    //接收完毕
+                       (PACKET1_MAX_LENGTH << RADIO_PCNF1_MAXLEN_Pos);
+		NRF_RADIO->EVENTS_END = 0U;//传输完成标志位，复位
+		NRF_RADIO->TASKS_START = 1U;//开始传输
+		while(NRF_RADIO->EVENTS_END == 0U)//接收完毕
     {
         if(timeout == 0) return 0;
         timeout--;
         nrf_delay_ms(1);
     }
-    if (NRF_RADIO->CRCSTATUS == 1U)       //CRC校验通过
+    if (NRF_RADIO->CRCSTATUS == 1U)//CRC校验通过
     {
         return 1;
     }
@@ -127,28 +127,28 @@ void Radio_Configure(void)
 {
     // Radio config
     NRF_RADIO->TXPOWER = (RADIO_TXPOWER_TXPOWER_Pos4dBm << RADIO_TXPOWER_TXPOWER_Pos);
-    NRF_RADIO->FREQUENCY = 7UL;                // Frequency bin 7, 2407MHz
+    NRF_RADIO->FREQUENCY = 2UL;                // Frequency bin 2, 2402MHz
     NRF_RADIO->MODE = (RADIO_MODE_MODE_Nrf_1Mbit << RADIO_MODE_MODE_Pos);
 
     // Radio address config
-    NRF_RADIO->PREFIX0 = NRF_FICR->DEVICEADDR[1];  // Prefix byte of addresses 3 to 0
+    NRF_RADIO->PREFIX0 = NRF_FICR->DEVICEADDR[0];  // Prefix byte of addresses 3 to 0
     NRF_RADIO->PREFIX1 = 0xFFFFFFFFUL;  // Prefix byte of addresses 7 to 4
-    NRF_RADIO->BASE0   = NRF_FICR->DEVICEADDR[0];  // Base address for prefix 0
+    NRF_RADIO->BASE0   = NRF_FICR->DEVICEADDR[1];  // Base address for prefix 0
     NRF_RADIO->BASE1   = 0xFFFFFFFFUL;  // Base address for prefix 1-7
     NRF_RADIO->TXADDRESS = 0x00UL;      // Set device address 0 to use when transmitting
-    NRF_RADIO->RXADDRESSES = 0x03UL;    // Enable device address 0 & 1 to use which receiving
+    NRF_RADIO->RXADDRESSES = 0x11UL;    // Enable device address 0 & 4 to use which receiving
 
     // Packet configuration
     NRF_RADIO->PCNF0 = (PACKET0_S1_SIZE << RADIO_PCNF0_S1LEN_Pos) |
                        (PACKET0_S0_SIZE << RADIO_PCNF0_S0LEN_Pos) |
-                       (PACKET0_PAYLOAD_SIZE << RADIO_PCNF0_LFLEN_Pos);
+                       (PACKET0_LEN_SIZE << RADIO_PCNF0_LFLEN_Pos);
 
     // Packet configuration
     NRF_RADIO->PCNF1 = (RADIO_PCNF1_WHITEEN_Disabled << RADIO_PCNF1_WHITEEN_Pos)    |
                        (RADIO_PCNF1_ENDIAN_Big << RADIO_PCNF1_ENDIAN_Pos)           |
                        (PACKET1_BASE_ADDRESS_LENGTH << RADIO_PCNF1_BALEN_Pos)       |
-                       (PACKET1_STATIC_LENGTH << RADIO_PCNF1_STATLEN_Pos)           |
-                       (PACKET1_PAYLOAD_SIZE << RADIO_PCNF1_MAXLEN_Pos);
+                       (PACKET1_PAYLOAD_SIZE << RADIO_PCNF1_STATLEN_Pos)           |
+                       (PACKET1_MAX_LENGTH << RADIO_PCNF1_MAXLEN_Pos);
 
     // CRC Config
     NRF_RADIO->CRCCNF = (RADIO_CRCCNF_LEN_Two << RADIO_CRCCNF_LEN_Pos); // Number of checksum bits
